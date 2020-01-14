3047cd241ece91dad6eb7cba1fc9f22705494e52
DAP <58054581+Dlray89@users.noreply.github.com>
2020-01-14 00:29:05 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -44,9 +44,94 @@ logo.setAttribute('src', siteContent["nav"]["img-src"]);
 let header = document.getElementById("cta-img");
 header.setAttribute('src', siteContent["cta"]["img-src"])
 
+
+//navBar///
+const nav1 = document.querySelectorAll('a');
+nav1[0].textContent = siteContent["nav"]["nav-item-1"];
+
+const nav2 = document.querySelectorAll('a');
+nav2[1].textContent = siteContent["nav"]["nav-item-2"];
+
+const nav3 = document.querySelectorAll('a');
+nav3[2].textContent = siteContent["nav"]["nav-item-3"];
+
+const nav4 = document.querySelectorAll('a');
+nav4[3].textContent = siteContent["nav"]["nav-item-4"];
+
+const nav5 = document.querySelectorAll('a');
+nav5[4].textContent = siteContent["nav"]["nav-item-5"];
+
+const nav6 = document.querySelectorAll('a');
+nav6[5].textContent = siteContent["nav"]["nav-item-6"]
+
+/////ends navBar///
+
+////cta box///////
+const grabCTA = document.querySelectorAll('h1');
+grabCTA[0].textContent = siteContent["cta"]["h1"];
+
+const getButton = document.querySelector('button');
+getButton.textContent = siteContent["cta"]["button"];
+
 let middleImg = document.getElementById("middle-img");
 middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 
+//////ends cta box//////
 
-let navBar = document.getElementsByTagName("a")
-navBar.
+/////main content//////
+
+const grabFeature = document.querySelectorAll('h4');
+grabFeature[0].textContent = siteContent["main-content"]["features-h4"]
+
+const grabFeatureP = document.querySelectorAll('p');
+grabFeatureP[0].textContent = siteContent["main-content"]["features-content"];
+
+const grabAbout = document.querySelectorAll('h4');
+grabAbout[1].textContent = siteContent["main-content"]["about-h4"];
+
+const grabAboutP = document.querySelectorAll('p');
+grabAboutP[1].textContent = siteContent["main-content"]["about-content"];
+
+///bottom-content/////
+
+const grabServices = document.querySelectorAll('h4');
+grabServices[2].textContent = siteContent["main-content"]["services-h4"];
+
+
+const grabServicesP = document.querySelectorAll('p');
+grabServicesP[2].textContent = siteContent["main-content"]["services-content"];
+
+const grabProduct = document.querySelectorAll('h4');
+grabProduct[3].textContent = siteContent["main-content"]["product-h4"];
+
+const grabProductP = document.querySelectorAll('p');
+grabProductP[3].textContent = siteContent["main-content"]["product-content"];
+
+const grabVision = document.querySelectorAll('h4');
+grabVision[4].textContent = siteContent["main-content"]["vision-h4"];
+
+const grabVisionP = document.querySelectorAll('p');
+grabVisionP[4].textContent = siteContent["main-content"]["vision-content"];
+//////closes bottom-content///////
+
+//////contact////////
+
+const grabContact = document.querySelectorAll('h4');
+grabContact[5].textContent = siteContent["contact"]["contact-h4"];
+
+
+const grabContactP = document.querySelectorAll('p');
+grabContactP[5].textContent = siteContent["contact"]["address"];
+
+const grabContactP2 = document.querySelectorAll('p');
+grabContactP2[6].textContent = siteContent["contact"]["phone"];
+
+const grabContactP3 = document.querySelectorAll('p');
+grabContactP3[7].textContent = siteContent["contact"]["email"];
+
+/////ends contacts/////////////
+
+////////footer/////////////////
+
+const grabFooter = document.querySelectorAll('p');
+grabFooter[8].textContent = siteContent["footer"]["copyright"];
